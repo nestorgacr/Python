66bc04e579d70e2c7bf37d77eec220012738e729
nestorgacr <nestorgacr@gmail.com>
2018-08-02 21:33:49 -0600

    Factorial, recursividad

---
+++ factorial.py
@@ -0,0 +1,12 @@
+def main():		
+	pass
+
+def factorial(number):
+    if number == 0:
+        return 1
+    return number * factorial(number -1)
+
+if __name__ == '__main__':
+    number = int(input('Escribe un numero: '))
+    result = factorial(number)
+    print(result)
